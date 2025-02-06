d2565e006e612d00989b63329123f6d37a29758f
Sam Whillance <samwhillance@gmail.com>
2025-02-07 10:05:47 +1100

    Update h3.tsx

--- src/components/h3.tsx
+++ src/components/h3.tsx
@@ -1,6 +1,6 @@
 
 export default function H3({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <h3 className={`text-xl font-bold mb-4 ${className}`}>{children}</h3>
+        <h3 className={`text-xl font-bold mt-4 mb-2 ${className}`}>{children}</h3>
     )
 }
