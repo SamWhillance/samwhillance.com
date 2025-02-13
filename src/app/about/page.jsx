502aceabbb81f56cc1a4e1830f3f4180e5ad3cd5
Sam Whillance <samwhillance@gmail.com>
2025-02-13 14:28:14 +1100

    Who i follow

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,7 +14,7 @@ export default function About() {
 
 					<div className="flex flex-row gap-2 items-center">
 						<Image src="/breville.jpg" alt="Breville" width={50} height={100} className="rounded-full mr-4" />
-						<a href="https://www.facebook.com/profile.php?id=100083324981791" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline hover:text-blue-800">Bendigo Breville Repairs & Service</a>
+						<a href="https://bendigobreville.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline hover:text-blue-800">Bendigo Breville Repairs & Service</a>
 					</div>
 
 					<div className="flex justify-center w-full mt-4">
@@ -25,9 +25,32 @@ export default function About() {
 							<GalleryImage src="/drink.png" />
 						</div>
 					</div>
+
+					<div className="flex flex-col gap-2 items-center mt-12">
+						<h2 className="text-2xl font-bold mb-4">Who I follow</h2>
+						<div className="flex flex-row flex-wrap gap-12 items-center text-center justify-center">
+							<FollowImage name="Fireship" src="/fireship.jpg" href="https://www.youtube.com/@Fireship" alt="Fireship" />
+							<FollowImage name="Theo" src="/theo.jpg" href="https://www.youtube.com/@t3dotgg" alt="Theo" />
+							<FollowImage name="Levels.io" src="/levels_io.jpg" href="https://x.com/levelsio" alt="Levels.io" />
+							<FollowImage name="Matt Vid Pro" src="/mattvid.jpg" href="https://www.youtube.com/@MattVidPro" alt="Matt Vid Pro" />
+							<FollowImage name="WDS" src="/web_dev_simplified.jpg" href="https://www.youtube.com/@WebDevSimplified" alt="WebDevSimplified" />
+							<FollowImage name="Kevin Powell" src="/kevin.jpg" href="https://www.youtube.com/@KevinPowell" alt="Kevin Powell" />
+						</div>
+					</div>
 				</div>
 			</ContentContainer>
 		</PageContainer>
+	)
+}
+
+const FollowImage = ({ name, src, href, alt }) => {
+	return (
+		<div className="flex flex-col gap-2 items-center">
+			<a href={href} target="_blank" rel="noopener noreferrer">
+				<Image src={src} alt={alt} width={64} height={64} className="rounded-full" />
+			</a>
+			<span className="text-sm font-bold">{name}</span>
+		</div>
 	)
 }
 
